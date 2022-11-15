60ee5a38203fb7f0f1fc806221e29009914d9d68
Ramiro González Maciel <ramirogm@gmail.com>
2022-11-14 21:44:04 -0300

    Add WebResource as Type

--- test/S3StorageAdapterTest.js
+++ test/S3StorageAdapterTest.js
@@ -3,15 +3,17 @@ import { S3StorageAdapter } from '../src/storage-adapters/S3StorageAdapter';
 // TODO: mock with https://aws.amazon.com/blogs/developer/mocking-modular-aws-sdk-for-javascript-v3-in-unit-tests/
 // https://github.com/m-radzikowski/aws-sdk-client-mock
 
+process.env.S3_ENDPOINT = 'http://localhost:43680';
+process.env.S3_ACCESS_KEY = 'AKIAIOSFODNN7EXAMPLE';
+process.env.S3_SECRET_KEY = 'wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY';
+
+const hex = '62616C656E6142414C454E41';
+const buf = Buffer.from(hex, 'hex');
+const filename = 'logo.png';
+
 describe('S3StorageAdapter', () => {
 	it('should put an object', async () => {
-		process.env.S3_ENDPOINT = 'http://localhost:43680';
-		process.env.S3_ACCESS_KEY = 'AKIAIOSFODNN7EXAMPLE';
-		process.env.S3_SECRET_KEY = 'wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY';
 		const adapter = new S3StorageAdapter();
-		const hex = '5261777221';
-		const buf = Buffer.from(hex, 'hex');
-		const filename = 'pepe.png';
 		const webresource = await adapter.saveFile(filename, buf);
 		console.log(webresource);
 	});
@@ -21,13 +23,7 @@ describe('S3StorageAdapter', () => {
 	});
 
 	it('should put + get an object', async () => {
-		process.env.S3_ENDPOINT = 'http://localhost:43680';
-		process.env.S3_ACCESS_KEY = 'AKIAIOSFODNN7EXAMPLE';
-		process.env.S3_SECRET_KEY = 'wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY';
 		const adapter = new S3StorageAdapter();
-		const hex = '5261777221';
-		const buf = Buffer.from(hex, 'hex');
-		const filename = 'pepe.png';
 		const webresource = await adapter.saveFile(filename, buf);
 		console.log(webresource);
 

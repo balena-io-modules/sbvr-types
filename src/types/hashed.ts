9bf8e6705d8062a09c8ab1ddc18aebefb4dece1f
myarmolinsky <yarmolinskymatthew@gmail.com>
2025-01-21 15:36:53 -0500

    Replace `bcrypt` with `crypto`
    Change-type: patch

--- src/types/hashed.ts
+++ src/types/hashed.ts
@@ -1,13 +1,5 @@
-import type * as Bcrypt from 'bcrypt';
-
+import * as crypto from 'crypto';
 import * as TypeUtils from '../type-utils';
-
-let bcrypt: typeof Bcrypt;
-try {
-	bcrypt = require('bcrypt');
-} catch {
-	bcrypt = require('bcryptjs');
-}
 
 export const types = {
 	postgres: 'CHAR(60)',
@@ -26,11 +18,29 @@ export const validate: TypeUtils.Validate<Types['Write'], DbWriteType> =
 		if (typeof value !== 'string') {
 			throw new Error('is not a string');
 		}
-		const salt = await bcrypt.genSalt();
-		return bcrypt.hash(value, salt);
+		const salt = crypto.randomBytes(16).toString('hex');
+		return new Promise((resolve, reject) => {
+			crypto.pbkdf2(value, salt, 1000, 64, 'sha512', (err, derivedKey) => {
+				if (err) {
+					reject(err);
+				}
+				resolve(derivedKey.toString('hex'));
+			});
+		});
 	});
 
-export const compare: (
-	data: string | Buffer,
-	encrypted: string,
-) => Promise<boolean> = bcrypt.compare.bind(bcrypt);
+export const compare = (data: string | Buffer, encrypted: string) => {
+	return new Promise<boolean>((resolve, reject) => {
+		const [storedHash, storedSalt] = encrypted.split('$');
+		if (!storedSalt) {
+			reject(new Error('Invalid hash'));
+			return;
+		}
+		crypto.pbkdf2(data, storedSalt, 1000, 64, 'sha512', (err, derivedKey) => {
+			if (err) {
+				return reject(err);
+			}
+			resolve(storedHash === derivedKey.toString('hex'));
+		});
+	});
+};
